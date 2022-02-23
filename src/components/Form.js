dc6af0c5f95001bb01fd97dbd204819eb5ae8bd7
kerley <kerley.kls@gmail.com>
2022-02-23 19:41:23 -0300

    voltando versao sem componetes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Input from './Input';
-import SelectComp from './SelectComp';
 
 class Form extends Component {
   render() {
@@ -8,30 +6,53 @@ class Form extends Component {
       <div className="box_form">
         <h1>Adicionar carta </h1>
         <form className="newcardForm">
-          <Input name="nome" label="nome" tipo="text" test="name-input" />
-          <Input
-            tipo="textarea"
-            name="descricao"
-            label="descricao"
-            test="description-input"
-          />
+          <label htmlFor="nome">
+            Nome:
+            <input type="text" name="nome" id="nome" data-testid="name-input" />
+          </label>
+          <label htmlFor="Descricao">
+            Descricao
+            <input
+              type="textarea"
+              name="descricao"
+              data-testid="description-input"
+              cols="30"
+              rows="10"
+            />
+          </label>
           <div className="inputAtributos">
-            <Input tipo="number" name="atr1" label="attr1" test="attr1-input" />
-            <Input tipo="number" name="atr2" label="attr2" test="attr2-input" />
-            <Input tipo="number" name="atr3" label="attr3" test="attr3-input" />
+            <label htmlFor="atr1">
+              Attr01
+              <input type="number" name="atr1" id="atr1" data-testid="attr1-input" />
+            </label>
+            <label htmlFor="atr2">
+              Attr02
+              <input type="number" name="atr2" id="atr2" data-testid="attr2-input" />
+            </label>
+            <label htmlFor="atr3">
+              Attr03
+              <input type="number" name="atr3" id="atr3" data-testid="attr3-input" />
+            </label>
             <p>Pontos restantes = 000</p>
           </div>
           <div className="inputUrl">
-            <Input tipo="text" name="imagem" label="imagem" test="image-input" />
+            <label htmlFor="imagem">
+              Imagem
+              <input type="number" name="imagem" id="imagem" data-testid="image-input" />
+            </label>
           </div>
           <div className="inputRariade">
-            <SelectComp name="raridade" label="raridade" test="rare-input" />
-            <Input
-              tipo="checkbox"
-              name="trunf"
-              label2="Super Trunfo"
-              test="trunfo-input"
-            />
+            <label htmlFor="raridade">
+              <select id="raridade" data-testid="rare-input">
+                <option value="normal">Normal</option>
+                <option value="raro">Raro</option>
+                <option value="muito raro">Muito Raro</option>
+              </select>
+            </label>
+            <label htmlFor="trunf">
+              <input type="checkbox" name="trunf" id="trunf" data-testid="trunfo-input" />
+              Super Trunfo
+            </label>
           </div>
           <button type="submit" data-testid="save-button">Salvar</button>
         </form>
